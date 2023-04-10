0a47b9be139c2960e507432cc96ec7ecf1004258
colac1 <hugof_lacerda@hotmail.com>
2023-04-10 11:02:33 +0100

    json variables working

---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "env" {
+  type        = map(string)
+  description = "Specifies the env of the resource, can be used in the naming."
+}
--- vars.tfvars
+++
@@ -1,3 +0,0 @@
-variable "env" {
-    description = "Specifies the env of the resource, can be used in the naming."
-}
